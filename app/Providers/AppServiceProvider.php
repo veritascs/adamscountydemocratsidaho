c84b387065dd6023cecaea6fa59c4928c0faf641
Max Bechdel <max.bechdel@advictoriamsolutions.com>
2026-04-19 10:13:28 -0600

    removing unused files/code for contact submissions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use App\Actions\MarkSubmissionReviewed;
-use App\Actions\MarkSubmissionUnreviewed;
 use Illuminate\Support\ServiceProvider;
-use Statamic\Statamic;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        MarkSubmissionReviewed::register();
-        MarkSubmissionUnreviewed::register();
-
         // Statamic::vite('app', [
         //     'resources/js/cp.js',
         //     'resources/css/cp.css',
